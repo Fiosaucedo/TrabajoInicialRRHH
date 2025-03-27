cd93830378cb8ec80e8f44b9ef758c65e6fe1d18
lucadi4z <luca-diaz@live.com>
2025-03-27 16:17:44 -0300

    cambio version de sklearn && agrego onehot encoding a campo habilidades

--- SLMachineLearning.py
+++ SLMachineLearning.py
@@ -1,31 +1,39 @@
 import pandas as pd
-from pandas import read_excel
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, MultiLabelBinarizer
 from sklearn.tree import DecisionTreeClassifier
 
-#Revisar en la docu de scikit learn LabelEncoder que sirve para transformar labels no numéricos en numéricos.
-#ej le.transform(["tokyo", "tokyo", "paris"])
-#   array([2, 2, 1]...)
-# o sea, tokyo vale 2 y paris 1.
-
-candidatos = { #Back
+candidatos = {
     'Nombre': ["Juan", "Pedro", "Maria", "Ana"],
     'Apellido': ["Perez", "Garcia", "Garcia", "Garcia"],
     'Años de experiencia': [1, 5, 3, 13],
-    'Habilidades': [1, 1, 1.5, 3],
+    'Nivel educativo': ["secundario", "universitario", "universitario", "universitario"],
+    'Habilidades': ["python, java", "c++, docker", "java, sql", "python, java, c++"],
     'Edad': [25, 30, 20, 25],
     'Apto': ["No", "Si", "No", "Si"]
 }
-df = pd.DataFrame(candidatos)
+data_frame = pd.DataFrame(candidatos)
 
-X = df[['Años de experiencia', 'Nivel educativo', 'Habilidades']]
-y = df['Apto']
+labelEncoderEducativo = LabelEncoder()
+labelEncoderApto = LabelEncoder()
+data_frame['Nivel educativo'] = labelEncoderEducativo.fit_transform(data_frame['Nivel educativo'])
+data_frame['Apto'] = labelEncoderApto.fit_transform(data_frame['Apto'])
+
+data_frame['Habilidades'] = data_frame['Habilidades'].apply(lambda x: x.split(', '))
+mlb = MultiLabelBinarizer()
+habilidades_encoded = pd.DataFrame(mlb.fit_transform(data_frame['Habilidades']), columns=mlb.classes_)
+
+data_frame = pd.concat([data_frame, habilidades_encoded], axis=1).drop(columns=['Habilidades'])
+
+X = data_frame.drop(columns=['Nombre', 'Apellido', 'Edad', 'Apto'])
+y = data_frame['Apto']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 modelo = DecisionTreeClassifier()
 modelo.fit(X_train, y_train)
 
-
 y_pred = modelo.predict(X_test)
-print(y_pred)
+y_pred_texto = labelEncoderApto.inverse_transform(y_pred)
+
+print(y_pred_texto)
